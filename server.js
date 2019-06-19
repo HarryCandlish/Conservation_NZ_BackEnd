c5972342ea563e512344c49cbd6593a824536d39
HarryCandlish <hcnd90@gmail.com>
2019-06-19 21:44:18 +1200

    added a few things to server.js

--- server.js
+++ server.js
@@ -7,20 +7,44 @@ app.get(`/api/tshirts`, (req, res) => {
     {
       id: 1,
       type: "T-Shirt",
-      slogan: "'Hope and All - All that Jazz'",
-      price: "$20"
+      slogan: "'Hope and All...'",
+      price: "$20",
+      size: "M, L, S"
     },
     {
       id: 2,
       type: "Longsleeve",
       slogan: "'Nothing Init'",
-      price: "$20"
+      price: "$20",
+      size: "M, L, S"
     },
     {
       id: 3,
       type: "T-Shirt",
-      slogan: "'Live with Your Ailments'",
-      price: "$20"
+      slogan: "'Live with Ailments'",
+      price: "$20",
+      size: "M, L, S"
+    },
+    {
+      id: 4,
+      type: "T-Shirt",
+      slogan: "'Ain't No Thing",
+      price: "$20",
+      size: "M, L, S"
+    },
+    {
+      id: 5,
+      type: "Longsleeve",
+      slogan: "'Love Just Love'",
+      price: "$20",
+      size: "M, L, S"
+    },
+    {
+      id: 6,
+      type: "Longsleeve",
+      slogan: "'All Is Well'",
+      price: "$20",
+      size: "M, L, S"
     }
   ];
   res.json(tshirts);
