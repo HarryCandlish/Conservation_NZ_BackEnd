5a917a787086a18074339dcdbcb144ae573460df
HarryCandlish <hcnd90@gmail.com>
2019-06-22 14:22:09 +1200

    changes to data

--- seeds/events.js
+++ seeds/events.js
@@ -7,10 +7,9 @@ exports.seed = function(knex, Promise) {
       return knex("events").insert([
         {
           id: 1,
-          name: "Coffee Catchups",
-          location: "Raglan Roast",
-          description:
-            "Meetup with other Wellingtonians for casual conversation and good coffee, discuss a myriad of topics, this is a relaxed event and is open to avid coffee fans",
+          name: "Meetup",
+          location: "Customs",
+          description: "Coffee meetup",
           date: 1551598786956,
           is_open: "true",
           type: "Quick",
@@ -18,57 +17,54 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 2,
-          name: "Pottery Classes",
-          location: "Wellington Potters Association",
+          name: "Discussion",
+          location: "Midnight Expresso",
           description:
-            "Evening class which caters to different levels of competency, Friday night club is where you can come together and share wine, food and tips on pottery making",
+            "A discussion event for discussing the mental health crises in Wellington",
           date: 1551314863037,
           is_open: "true",
-          type: "Evening class",
+          type: "Evening discussion",
           image: "/"
         },
         {
           id: 3,
-          name: "Mountain Biking Club",
-          location: "Start at Mount Victoria",
+          name: "Lecture",
+          location: "Victoria University",
           description:
-            "Meet up with avid mountain bikers in Wellington, starting at the top of Mount Vic and hitting the trails from medium to hard, end the day with a beer or two and some chats",
+            "A lecture which talks through the history of mental illness",
           date: 1551314863037,
           is_open: "true",
-          type: "Whole day",
+          type: "Lecture",
           image: "/"
         },
         {
           id: 4,
-          name: "Language Exchange",
-          location: "Wellington Library",
-          description:
-            "Exchange languages with others, currently we have members from China, England and Germany! Come and pratice a language with us, meeting at Level One of the Library",
+          name: "Volunteering",
+          location: "Wellington",
+          description: "Volunteering around greater Wellington",
           date: 1551314863037,
           is_open: "true",
-          type: "Half day",
+          type: "Volunteering",
           image: "/"
         },
         {
           id: 5,
-          name: "Plant Exchange",
-          location: "Frank Kitts Park",
-          description:
-            "Plant exhange for those who are interested, come down to Frank Kitts park and bring some plants which you would like to exchange for other plants - mainly for those who propogate plants to share with others",
+          name: "Community Clean Up",
+          location: "Owhero Bay",
+          description: "Beach Clean",
           date: 1551314863037,
           is_open: "true",
-          type: "Morning",
+          type: "Volunteering",
           image: "/"
         },
         {
           id: 6,
-          name: "Table Tennis",
-          location: "Victoria University",
-          description:
-            "Table Tennis club is on every Tuesday, open to all levels just bring your bat",
+          name: "Art Gallery",
+          location: "Gallery",
+          description: "Art therapy",
           date: 1551314863037,
           is_open: "true",
-          type: "Afternoon",
+          type: "Art",
           image: "/"
         }
       ]);
