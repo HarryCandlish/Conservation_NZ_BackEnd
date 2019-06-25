1c7bb5404d472a3d46f9003fa4ad2739437b5474
HarryCandlish <hcnd90@gmail.com>
2019-06-25 15:12:13 +1200

    routes and db functions written for philosophy data

---
+++ routes/philosophy.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+const { getPhilosophies, getOnePhilosophy } = require("../db/products");
+
+router.get("/", (req, res) => {
+  getPhilosophies()
+    .then(philosophies => {
+      res.json(philosophies);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "error" });
+    });
+});
+
+router.get("/philosophy/:id", (req, res) => {
+  const philosophyId = req.params.id;
+  getOneProduct(philosophyId)
+    .then(philosophy => {
+      res.json(philosophy);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "error" });
+    });
+});
+
+module.exports = router;
