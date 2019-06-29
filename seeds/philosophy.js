88365c8e4a784e796d3dd8e6d7f4c5c720c82a15
HarryCandlish <hcnd90@gmail.com>
2019-06-29 12:00:51 +1200

    changes to data

--- seeds/philosophy.js
+++ seeds/philosophy.js
@@ -9,21 +9,22 @@ exports.seed = function(knex, Promise) {
           id: 1,
           title: "Sisyphus",
           details: "influenced by the work of Camus",
-          description: "One can image Sisyphus happy"
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
           id: 2,
           title: "All is Well",
           details: "",
           description:
-            "All is Well has a complex history throughout Western thought"
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
           id: 3,
-          title: "Mental Health in the current climate",
+          title: "Current Climate",
           details: "Mental Health in New Zealand",
           description:
-            "Mental health problem has to be face and it's key that we fight the imposing issue."
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
       ]);
     });
